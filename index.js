6aee3610b7211ca2bfd954660bb2e8510100facb
Larissa <larissa-a-g@hotmail.com>
2022-02-19 20:44:29 -0300

    modificando html,css,js

--- index.js
+++ index.js
@@ -3,10 +3,5 @@ function openP(question, answer, icons) {
         icon.classList.toggle('active');
         let p = document.getElementById(answer);
         p.classList.toggle('visible-faq');
-    if (p.classList.contains('visible-faq')){
-        imageClick.addEventListener('click', ()=>{
-            icon.classList.toggle('active')
-            p.classList.toggle('visible-faq')
-        })
-    }
+
 }
